51f1352fdf2c23cedca0cba7043718e6bd59400d
Ramnath R Iyer <rri@oxo-lang.org>
2024-05-25 23:03:43 -0700

    Add command-line arguments and interactive read-eval-loop-print command

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,15 +1,28 @@
 //! Top-level application and associated utilities.
 
 use crate::err::SysErr;
+use clap::{Parser, Subcommand};
 
 /// Application entry-point.
-#[derive(Clone, Debug)]
-pub struct App;
+#[derive(Clone, Debug, Parser)]
+#[command(about, name("oxo"), version, long_about(None))]
+pub struct App {
+    /// Command to execute.
+    #[command(subcommand)]
+    cmd: Cmd,
+}
+
+/// Command available to execute.
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Subcommand)]
+pub enum Cmd {
+    /// Launch an interactive read-eval-print-loop.
+    Repl,
+}
 
 impl App {
     /// Create a new instance.
     pub fn new() -> Self {
-        Self
+        Self::parse()
     }
 }
 
